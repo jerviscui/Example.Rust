2a9d30927494abcf83c43e92ab6102998d317255
jerviscui <cuijervis@126.com>
2024-01-08 07:33:22 +0800

    Add generic trait tests

--- src/trait_tests.rs
+++ src/trait_tests.rs
@@ -64,3 +64,94 @@ impl Add for &Complex {
         }
     }
 }
+
+pub struct GenericComplex<T> {
+    pub real: T,
+    pub imagine: T,
+}
+
+impl<T> Add for GenericComplex<T>
+where
+    T: Add<Output = T>,
+{
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        GenericComplex {
+            real: self.real + rhs.real,
+            imagine: self.imagine + rhs.imagine,
+        }
+    }
+}
+
+impl<T, RT> Add<RT> for GenericComplex<T>
+where
+    T: Add<RT, Output = T>,
+    RT: Copy,
+{
+    type Output = Self;
+    fn add(self, rhs: RT) -> Self::Output {
+        GenericComplex {
+            real: self.real + rhs,
+            imagine: self.imagine,
+        }
+    }
+}
+
+#[test]
+fn impl_trait_generic_struct() {
+    let c = GenericComplex::<f64> {
+        real: 1.0,
+        imagine: 2.0,
+    };
+
+    assert_eq!(c.add(1.0).real, 2.0);
+
+    let a = GenericComplex::<f64> {
+        real: 1.0,
+        imagine: 2.0,
+    };
+    let b = GenericComplex::<f64> {
+        real: 2.0,
+        imagine: 2.2,
+    };
+
+    assert_eq!(a.add(b).imagine, 4.2);
+
+    let d = GenericComplex::<i32> {
+        real: 1,
+        imagine: 2,
+    };
+
+    assert_eq!((d + 1).real, 2);
+}
+
+// impl<T> Add<T> for GenericComplex<T>
+// where
+//     T: Add<Output = T>,
+// {
+//     type Output = Self;
+//     fn add(self, rhs: T) -> Self::Output {
+//         GenericComplex {
+//             real: self.real + rhs,
+//             imagine: self.imagine,
+//         }
+//     }
+// }
+
+// #[test]
+// fn impl_trait_generic_struct_add_f64() {
+//     let c = GenericComplex::<f64> {
+//         real: 1.0,
+//         imagine: 2.0,
+//     };
+
+//     assert_eq!(c.add(1.0).real, 2.0);
+
+//     let d = GenericComplex::<i32> {
+//         real: 1,
+//         imagine: 2,
+//     };
+
+//     // error[E0308]: mismatched types
+//     assert_eq!(d.add(1).real, 2);
+// }
